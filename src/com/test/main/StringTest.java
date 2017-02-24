1823cd384056ce9cd0b609f0fa08ae76e487123a
wkaehfdl12 <wkaehfdl12@naver.com>
2017-02-24 17:38:16 +0900

    edit StringTest.java

--- src/com/test/main/StringTest.java
+++ src/com/test/main/StringTest.java
@@ -11,16 +11,23 @@ public class StringTest {
         String t = "!!";
         System.out.println("s 값 => "+s);
         System.out.println("t 값 => "+t);
-        
-        s = t;
-        
         System.out.println("s 주소 => "+s.getClass().getName() + '@' + Integer.toHexString(s.hashCode()));
         System.out.println("t 주소 => "+t.getClass().getName() + '@' + Integer.toHexString(t.hashCode()));
+        System.out.println();
+        System.out.println("s = t 선언");
+        System.out.println();
+        s = t;
+        
+        System.out.println("s 값 => "+s);
+        System.out.println("t 값 => "+t);
+        System.out.println("s 주소 => "+s.getClass().getName() + '@' + Integer.toHexString(s.hashCode()));
+        System.out.println("t 주소 => "+t.getClass().getName() + '@' + Integer.toHexString(t.hashCode()));
+
         //변경
         StringValueEdit.edit(s, "");
         //StringValueEdit.edit(t, "");
         
-        System.out.println("\r\n변경\r\n");
+        System.out.println("\r\nStringValueEdit.edit(s, \"\") 호출 \r\n");
         System.out.println("s 값 => "+s);
         System.out.println("t 값 => "+t);
         
@@ -35,28 +42,35 @@ public class StringTest {
         System.out.println();
         
         String s2 = "string!";
+        //!! 값이 메모리에 떠있나..
         String t2 = "!!";
         
         System.out.println("s2 값 => "+s2);
         System.out.println("t2 값 => "+t2);
+        System.out.println("s2 주소 => "+s2.getClass().getName() + '@' + Integer.toHexString(s2.hashCode()));
+        System.out.println("t2 주소 => "+t2.getClass().getName() + '@' + Integer.toHexString(t2.hashCode()));
+        System.out.println();
+        System.out.println("StringValueEdit.edit(t2, \"change!!\") 호출");
+        System.out.println();
         
+        StringValueEdit.edit(t2, "change!!");
+        
+        System.out.println("s2 값 => "+s2);
+        System.out.println("t2 값 => "+t2);
         System.out.println("s2 주소 => "+s2.getClass().getName() + '@' + Integer.toHexString(s2.hashCode()));
         System.out.println("t2 주소 => "+t2.getClass().getName() + '@' + Integer.toHexString(t2.hashCode()));
         //변경
         //StringValueEdit.notEdit(s2, "change Value22");
         //StringValueEdit.notEdit(t2, "change Value23");
         System.out.println("-----------------------------------------");
-        System.out.println("s2와 t2의 값을 각각 change Value1, change Value2로 세팅함");
-        s2 = "change Value1";
-        t2 = "change Value2";
-        
-        System.out.println("\r\n변경\r\n");
-        System.out.println("s2 값 => "+s2);
-        System.out.println("t2 값 => "+t2);
-        
-        System.out.println("s2 주소 => "+s2.getClass().getName() + '@' + Integer.toHexString(s2.hashCode()));
-        System.out.println("t2 주소 => "+t2.getClass().getName() + '@' + Integer.toHexString(t2.hashCode()));
+        System.out.println("s와 t의 값을 출력");
+        System.out.println();
+        System.out.println("s 값 => "+s);
+        System.out.println("t 값 => "+t);
+        System.out.println("s 주소 => "+s.getClass().getName() + '@' + Integer.toHexString(s.hashCode()));
+        System.out.println("t 주소 => "+t.getClass().getName() + '@' + Integer.toHexString(t.hashCode()));
     }
 }
+
 
 
